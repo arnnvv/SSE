09739cb16633c9ec0e9da2b69e8f24a76165b6d7
arnnvv <arnav.900@protonmail.com>
2024-12-08 22:00:31 +0530

    updt

---
+++ clnt/src/app/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect, JSX, useState } from "react";
+
+export default function Home(): JSX.Element {
+  const [messages, setMessages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const eventSource = new EventSource("http://localhost:3001/");
+
+    eventSource.onmessage = (event: MessageEvent) => {
+      setMessages((prev) => [...prev, event.data]);
+    };
+
+    eventSource.onerror = () => {
+      console.error("EventSource failed.");
+      eventSource.close();
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>SSE Client</h1>
+      <ul>
+        {messages.map((msg, index) => (
+          <li key={index}>{msg}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
